6372b839ec05e65f33b536d3e9e2b54b281360a3
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2020-04-25 17:51:29 +0300

    Nerfed fried eggs

--- src/main/java/com/markus1002/incubation/common/item/ModFoods.java
+++ src/main/java/com/markus1002/incubation/common/item/ModFoods.java
@@ -4,6 +4,6 @@ import net.minecraft.item.Food;
 
 public class ModFoods
 {
-	public static final Food FRIED_EGG = (new Food.Builder()).hunger(4).saturation(0.6F).build();
+	public static final Food FRIED_EGG = (new Food.Builder()).hunger(3).saturation(0.6F).build();
 	public static final Food SCRAMBLED_EGGS = (new Food.Builder()).hunger(6).saturation(0.6F).build();
 }
